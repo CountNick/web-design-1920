2f9814902c5932b3084d73f2f7cd3b16c790958d
CountNick <meijer.nick1@gmail.com>
2020-04-15 11:13:43 +0200

    Oscillerator now working

---
+++ collegeMarie.md
@@ -0,0 +1,27 @@
+# Dovencultuur
+
+Marie had het in het begin van haar college over 2 verschillende soorten doof zijn namelijk: 
+
+* cultureel dove mensen
+
+* mensen die doof zijn vanuit een medischperspectief
+
+# Frustraties
+
+Vaak komt Marie een website tegen waar zij haar telefoonnummer in moet vullen voor verdere hulp. Dit is voor haar natuurlijk erg vervelend omdat zij niet kan horen. Zo vertelde zij over een paar vervelende situaties die dit veroorzaakte:
+
+* Zo had zij bij de ns een telefoongesprek waar zij een doven tolk aan de videochat had om haar abonnement te vernieuwen. Maar toch moest zij om toestemming te geven om haar abonnement te verlengen zelf toestemming geven. Zij dacht dit te doen doormiddel van haa tolk maar dit was blijkbaar niet voldoende. Zij moest zelf haar stem gebruiken om te zeggen dat dit oke was. Zij vertelde dat zij hierdoor het gevoel kreeg dat gebarentaal eigenlijk niet telt als taal.
+
+* Op een adnere website moest zij haar nummer invullen om in contact te komen met een bedrijf. Wat van te voren al frustrerend voor haar is want zij moet dan van te voren een tolk regelen etc. Op een gegeven moment werd zij dus door een nummer gebeld waarvan zij dacht dat dit belnagrijk zou zijn. Het nummer heeft haar vaak gebeld maar toen zij uitendelijk een tolk had geregeld kreeg de tolk het nummer niet te pakken. Uitendelijk bleek dit een verzekeringsmaatschappij te zijn en was het helemaal geen belangrijk telefoontje
+
+# Ontwerpen voor doven
+
+Tijdens het college van Marie zijn wij veel dingen tegen gekomen over het ontwerpen voor dove mensen. Zo liet zij een video zien van twee jongens die handschoenen hadden gecreeërd die gebarentaal zou moeten omzetten naar gesproken taal. Hier was een boel op aan te merken namelijk: Hoe zou degene die terugpraat dan vertaald worden naar de persoon die doof is? Hoe weet je of dit unerhaupt bruikbaar is voor dove mensen. De jongens hadden dus helemaal geen onderzoek gedaan bij de behoefte en hebben het prototype eigenlijke compleet op bias gemaakt. Dit is natuurlijk niet de bedoeling, het is de bedoeling dat je juist met de doelgroep in gesprek gaat en weet waar de pijnpunten zitten dbinnen een interactie.
+
+# Hoe ontwerp je beter voor dove mensen?
+
+* Korte zinnen
+* Grotere teksten
+* Snel tot de kern komen
+
+Marie vertelde ook dat je hiermee niet alleen dove mensen helpt maar ook mensen die laaggeletterd zijn.
---
+++ js/index.js
@@ -0,0 +1,232 @@
+//https://stackoverflow.com/questions/9419263/playing-audio-with-javascript
+
+const audio = new Audio()
+
+const playList = new Array('./audio/all-samples/banjo/banjo_E3_very-long_forte_normal.mp3', './audio/all-samples/banjo/banjo_F3_very-long_forte_normal.mp3', './audio/all-samples/banjo/banjo_Fs3_very-long_piano_normal.mp3', './audio/all-samples/banjo/banjo_G3_very-long_piano_normal.mp3', './audio/all-samples/banjo/banjo_Gs4_very-long_forte_normal.mp3', './audio/all-samples/banjo/banjo_Gs4_very-long_piano_normal.mp3', './audio/all-samples/banjo/banjo_Gs5_very-long_piano_normal.mp3', './audio/all-samples/banjo/banjo_E5_very-long_piano_normal.mp3', './audio/all-samples/banjo/banjo_E6_very-long_forte_normal.mp3')
+
+
+
+const data = [
+
+    {
+      name: "Avocado",
+      value: 10,
+    },
+
+    {
+      name: "Papaya",
+      value: 20,
+    },
+
+    {
+        name: "Pineapples",
+        value: 30,
+    },
+
+    {
+        name: "Mango's",
+        value: 40,
+    },
+
+    {
+        name: "Apples",
+        value: 50,
+    },
+
+    {
+        name: "Apricots",
+        value: 60,
+    },
+    {
+      name: "Icecream",
+      value: 70,
+    },
+    {
+      name: "Strawberries",
+      value: 80,
+    },
+    {
+        name: "Bananas",
+        value: 100,
+    }
+    
+
+  ]
+
+  const margin = { top: 20, right: 20, bottom: 70, left: 90 }
+  const width = 600 - margin.left - margin.right
+  const height = 300 - margin.top - margin.bottom
+  const legendWidth = 300
+
+//responsive: https://stackoverflow.com/questions/44833788/making-svg-container-100-width-and-height-of-parent-container-in-d3-v4-instead
+  const svg = d3
+    .select("#chart")
+    // .attr("width", width + margin.left + margin.right + legendWidth)
+    // .attr("height", height + margin.top + margin.bottom)
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "0 0 800 500")
+    .attr("aria-labelledby", "bar-chart-title")
+
+  svg
+    .append("text")
+    .attr("x", margin.left)
+    .attr("y", 250)
+    .text("2018 Fruit Production")
+    .attr("id", "bar-chart-title")
+
+  svg
+    .append("text")
+    .text("Legend")
+    .attr("x", width + margin.right + margin.left)
+    .attr("y", 20)
+
+  svg
+    .append("text")
+    .text("B = billion")
+    .attr("x", width + margin.right + margin.left)
+    .attr("y", 40)
+
+  const xValues = []
+  data.map((datum) => xValues.push(datum.name))
+
+  const xScale = d3
+    .scaleBand()
+    .domain(xValues)
+    .range([0, width])
+    .padding(0.1)
+
+  const yValues = []
+  data.map((datum) => yValues.push(datum.value))
+
+  const yScale = d3
+    .scaleLinear()
+    .domain([0, d3.max(yValues)])
+    .nice()
+    .range([height, 0])
+
+  const barGroups = svg
+    .append("g")
+    .attr("role", "list")
+    .attr("aria-label", "bar chart")
+    .attr("class", "data")
+    .attr("transform", `translate(${margin.left}, 0)`)
+
+  const barColors = ["#ff0000", "#fffb00", "#002fff", "#ff7b00", "#00ff04", "#9000ff", "#ff00bb", "#fffffff"]
+
+  const g = barGroups
+    .selectAll("g")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("role", "listitem")
+
+  g.append("rect")
+    .attr("fill", (d, i) => barColors[i])
+    .attr("stroke", "#000")
+    .attr("class", "bar")
+    .attr("x", (d) => xScale(d.name))
+    .attr("y", (d) => yScale(d.value))
+    .attr("width", xScale.bandwidth())
+    .attr("height", (d) => height - yScale(d.value))
+    .on('focus', (d, i) => handleBarFocus(d, i))
+    .on('blur', function () {
+        d3.select(this)
+            .classed('highlighted', false)
+            .attr('tabindex', '-1');
+    });
+
+  const lineItemHeight = 30
+
+  g.append("rect")
+    .attr("fill", (d, i) => barColors[i])
+    .attr("stroke", "#000")
+    .attr("width", 20)
+    .attr("height", 20)
+    .attr("x", width + margin.right)
+    .attr("y", (d, i) => lineItemHeight * (i + 1) + 30)
+
+  g.append("text")
+    .text(
+      (d) => `${d.name} - ${d3.format(".2s")(d.value).replace("G", "B")}`
+    )
+    .attr("x", width + margin.right + 30)
+    .attr("y", (d, i) => lineItemHeight * (i + 1) + 45)
+
+  const xAxis = d3.axisBottom(xScale).tickSize(0)
+
+  svg
+    .append("g")
+    .attr("class", "x-axis")
+    .attr("aria-label", "x axis")
+    .attr("transform", `translate(${margin.left}, ${height})`)
+    .call(xAxis)
+
+  const increment = Math.ceil(d3.max(yValues) / 5)
+  const tickRange = d3.range(0, d3.max(yValues), increment)
+
+  var yAxis = d3
+    .axisLeft(yScale)
+    .tickValues(tickRange)
+    .tickFormat((d) => d3.format(".2s")(d).replace("G", "B"))
+
+  svg
+    .append("g")
+    .attr("class", "y-axis")
+    .attr("aria-label", "y axis")
+    .attr("transform", `translate(${margin.left}, 0)`)
+    .call(yAxis)
+
+const bars = d3.selectAll('.bar').nodes()
+let highlightedBarIndex = null
+
+
+console.log('bars: ', bars)
+
+function handleArrowKey(){
+    const pushed = d3.event.keyCode
+    if(pushed !== 37 && pushed !== 39) return
+    else console.log(pushed)
+
+    if(pushed === null){
+        console.log(pushed)
+    }
+}
+
+const context = new AudioContext();
+
+function handleBarFocus(data, index){
+    highlightedBarIndex = index
+    const highlightedBar = d3.select(bars[index]);
+    const volumeScale = d3.scaleLinear().domain([100, 1500]).range([2, 0.3]);
+    console.log('hiero', highlightedBar)
+
+    // d3.selectAll(bars)
+    // .classed('highlighted', false)
+    // .attr('tabindex', '-1');
+
+    // highlightedBar
+    // .classed('highlighted', true)
+    // .attr('tabindex', '0');
+
+    // const self = this
+    // console.log('self: ', self)
+
+    highlightedBar.attr('d', function (d) {
+        // Audio functionality from: http://bl.ocks.org/aholachek/6e18a82c0f0ada144b854f788c07d7a4
+        console.log(d)
+        const oscillator = context.createOscillator();
+        const gainNode = context.createGain();
+        gainNode.gain.value = volumeScale(d.value);
+        oscillator.type = 'triangle';
+        oscillator.frequency.value = d.value; // Hz
+        // Connect the oscillator to our speakers after passing it
+        // through the gainNode to modulate volume
+        oscillator.connect(gainNode);
+        gainNode.connect(context.destination);
+        // Start the oscillator now
+        oscillator.start();
+        // this rapidly ramps sound down
+        gainNode.gain.setTargetAtTime(0, context.currentTime, 0.3);
+    });
+
+}
